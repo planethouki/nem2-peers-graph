6fc94d8e90f9327b5740f82456a40e94276f281c
planethouki <26595148+planethouki@users.noreply.github.com>
2020-01-05 22:40:11 +0900

    change fetch url

--- web/env-prod.js
+++ web/env-prod.js
@@ -1,8 +1,8 @@
 module.exports = {
     getGraphData() {
         return Promise.all([
-            fetch('https://tools48gh23s.z31.web.core.windows.net/peers-graph/data/nodes.json'),
-            fetch('https://tools48gh23s.z31.web.core.windows.net/peers-graph/data/edges.json')
+            fetch('https://tools48gh23s.blob.core.windows.net/data/peers-graph/nodes.json'),
+            fetch('https://tools48gh23s.blob.core.windows.net/data/peers-graph/edges.json')
         ])
             .then(([nodes, edges]) => {
                 return Promise.all([nodes.json(), edges.json()])
