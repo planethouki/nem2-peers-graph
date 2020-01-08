632b5ea1735d590e64d8d21e7d95d48db85cd71d
planethouki <26595148+planethouki@users.noreply.github.com>
2020-01-08 22:58:25 +0900

    webpack css extract chunk file name

--- web/webpack.prod.js
+++ web/webpack.prod.js
@@ -1,10 +1,34 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
 
 module.exports = merge(common, {
     mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+        ]
+    },
+    output: {
+        filename: '[name].min.js',
+        chunkFilename: '[hash].min.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
     plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].min.css',
+            chunkFilename: '[id].min.css',
+            ignoreOrder: false,
+        }),
         new webpack.ProvidePlugin({
             env: '../env-prod'
         })
