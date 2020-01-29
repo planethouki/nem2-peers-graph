f4cab9fbb6380e3b8dc920626c5473de5a94d759
planethouki <26595148+planethouki@users.noreply.github.com>
2020-01-29 09:50:15 +0900

    xhr json no cache

--- web/env-prod.js
+++ web/env-prod.js
@@ -1,15 +1,15 @@
 module.exports = {
     getGraphData() {
         return Promise.all([
-            fetch('https://tools48gh23s.blob.core.windows.net/data/peers-graph/nodes.json'),
-            fetch('https://tools48gh23s.blob.core.windows.net/data/peers-graph/edges.json')
+            fetch('https://tools48gh23s.blob.core.windows.net/data/peers-graph/nodes.json?_=' + Date.now()),
+            fetch('https://tools48gh23s.blob.core.windows.net/data/peers-graph/edges.json?_=' + Date.now())
         ])
             .then(([nodes, edges]) => {
                 return Promise.all([nodes.json(), edges.json()])
             })
     },
     getPeersData() {
-        return fetch('https://tools48gh23s.blob.core.windows.net/data/peers-graph/peers.json')
+        return fetch('https://tools48gh23s.blob.core.windows.net/data/peers-graph/peers.json?=_' + Date.now())
             .then((peers) => peers.json())
     }
 }
